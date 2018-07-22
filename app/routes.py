daeedf42b1ce6db442bf62ebd1f207a93fe6272f
Dan <danielcopelin@gmail.com>
2018-07-22 13:27:21 +1000

    Initial commit. Mid-way through Part III.

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Dan'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Dan'},
+            'body': 'Arghhh!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
